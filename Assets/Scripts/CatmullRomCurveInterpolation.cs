003bc3f673e03a4024ba6f3937fb91df6d95c4a0
Kevin Oliver <oliver.395@osu.edu>
2021-03-14 11:09:30 -0400

    interpolation working

--- Assets/Scripts/CatmullRomCurveInterpolation.cs
+++ Assets/Scripts/CatmullRomCurveInterpolation.cs
@@ -18,6 +18,7 @@ public class CatmullRomCurveInterpolation : MonoBehaviour {
 	const double DT = 0.01;
 
 	int segment_number;
+	
 	//GameObject tempcube;
 
 	/* Returns a point on a cubic Catmull-Rom/Blended Parabolas curve
@@ -27,56 +28,90 @@ public class CatmullRomCurveInterpolation : MonoBehaviour {
 	Vector3 ComputePointOnCatmullRomCurve(double u, int segmentNumber)
 	{
 		Vector3 point = new Vector3();
-		Vector3 p0;
-		Vector3 p1;
-		Vector3 p2;
-		Vector3 p3;
-		
-		if (segmentNumber == controlPoints.Length - 1)
+		int lastPoint = controlPoints.Length - 1;
+		// cant multipy vectors by doubles
+		float t = 0.5f;
+
+		//pi-2, pi-1, pi, pi+1
+		Vector3 pim2;
+		Vector3 pim1;
+		Vector3 pi;
+		Vector3 pip1;
+
+
+
+		//if (segmentNumber == lastPoint)
+		//{
+		//	p0 = controlPoints[segmentNumber-1];
+		//	p1 = controlPoints[segmentNumber];
+		//	p0p = t * (controlPoints[segmentNumber] - controlPoints[segmentNumber-2]);
+		//	p1p = t * (controlPoints[0] - controlPoints[segmentNumber-1]);
+		//}
+		//      else if(segmentNumber == 0)
+		//      {
+		//	p0 = controlPoints[lastPoint];
+		//	p1 = controlPoints[segmentNumber];
+		//	p0p = t * (controlPoints[segmentNumber] - controlPoints[lastPoint - 1]);
+		//	p1p = t * (controlPoints[segmentNumber + 1] - controlPoints[lastPoint]);
+		//}
+		//      else if(segmentNumber == 1)
+		//      {
+		//	p0 = controlPoints[segmentNumber - 1];
+		//	p1 = controlPoints[segmentNumber];
+		//	p0p = t * (controlPoints[segmentNumber] - controlPoints[lastPoint]);
+		//	p1p = t * (controlPoints[segmentNumber + 1] - controlPoints[segmentNumber - 1]);
+		//}
+		//      else
+		//      {
+		//	p0 = controlPoints[segmentNumber - 1];
+		//	p1 = controlPoints[segmentNumber];
+		//	p0p = t * (controlPoints[segmentNumber] - controlPoints[lastPoint]);
+		//	p1p = t * (controlPoints[segmentNumber + 1] - controlPoints[segmentNumber - 1]);
+		//}
+
+		if (segmentNumber == lastPoint)
 		{
-			p0 = controlPoints[segmentNumber-1];
-			p1 = controlPoints[segmentNumber];
-			p2 = controlPoints[0];
-			p3 = controlPoints[1];
+			pim2 = controlPoints[segmentNumber-2];
+			pim1 = controlPoints[segmentNumber - 1];
+			pi = controlPoints[segmentNumber];
+			pip1 = controlPoints[0];
 		}
-        else if(segmentNumber == controlPoints.Length - 2)
-        {
-			p0 = controlPoints[segmentNumber-1];
-			p1 = controlPoints[segmentNumber];
-			p2 = controlPoints[segmentNumber + 1];
-			p3 = controlPoints[0];
-		}
-		else if (segmentNumber == controlPoints.Length - 3)
+		else if (segmentNumber == 0)
 		{
-			p0 = controlPoints[segmentNumber - 1];
-			p1 = controlPoints[segmentNumber];
-			p2 = controlPoints[segmentNumber + 1];
-			p3 = controlPoints[segmentNumber + 2];
-        }
-        else if(segmentNumber == 0)
-        {
-			p0 = controlPoints[controlPoints.Length - 1];
-			p1 = controlPoints[segmentNumber];
-			p2 = controlPoints[segmentNumber + 1];
-			p3 = controlPoints[segmentNumber + 2];
-        }
-        else
-        {
-			p0 = controlPoints[segmentNumber - 1];
-			p1 = controlPoints[segmentNumber];
-			p2 = controlPoints[segmentNumber + 1];
-			p3 = controlPoints[segmentNumber + 2];
+			pim2 = controlPoints[lastPoint - 1];
+			pim1 = controlPoints[lastPoint];
+			pi = controlPoints[segmentNumber];
+			pip1 = controlPoints[segmentNumber + 1];
+		}
+		else if (segmentNumber == 1)
+		{
+			pim2 = controlPoints[lastPoint];
+			pim1 = controlPoints[segmentNumber - 1];
+			pi = controlPoints[segmentNumber];
+			pip1 = controlPoints[segmentNumber + 1];
+		}
+		else
+		{
+			pim2 = controlPoints[segmentNumber - 2];
+			pim1 = controlPoints[segmentNumber - 1];
+			pi = controlPoints[segmentNumber];
+			pip1 = controlPoints[segmentNumber + 1];
 		}
 
 
 		//The coefficients of the cubic polynomial (except the 0.5f * which I added later for performance)
-		Vector3 a = 0.5f * 2f * p1;
-		Vector3 b = 0.5f * p2 - p0;
-		Vector3 c = 0.5f * 2f * p0 - 5f * p1 + 4f * p2 - p3;
-		Vector3 d = 0.5f * -p0 + 3f * p1 - 3f * p2 + p3;
+		//Vector3 a = 0.5f * 2f * p1;
+		//Vector3 b = 0.5f * p0p - p0;
+		//Vector3 c = 0.5f * 2f * p0 - 5f * p1 + 4f * p0p - p1p;
+		//Vector3 d = 0.5f * -p0 + 3f * p1 - 3f * p0p + p1p;
+
+		Vector3 c0 = pim1;
+		Vector3 c1 = (-t) * pim2 + (t) * pi;
+		Vector3 c2 = (2*t * pim2) + ((t-3) * pim1) + ((3 - 2*t) * pi) + ((-t) *  pip1);
+		Vector3 c3 = ((-t) * pim2) + ((2 - t) * pim1) + ((t - 2) * pi) + (t * pip1);
 
 		//The cubic polynomial: a + b * u + c * u^2 + d * u^3
-		point = (a + (b * (float)u) + (c * (float)u * (float)u) + (d * (float)u * (float)u * (float)u));
+		point = (c0 + (c1 * (float)u) + (c2 * (float)u * (float)u) + (c3 * (float)u * (float)u * (float)u));
 
 
 		// TODO - compute and return a point as a Vector3		
@@ -125,17 +160,20 @@ public class CatmullRomCurveInterpolation : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+		int num = 2;
+
 		time += DT;
-        if (Vector3.Distance(transform.position, controlPoints[segment_number]) > 0.1)
-        {
-            Vector3 temp = ComputePointOnCatmullRomCurve(DT, segment_number);
+        //if (Vector3.Distance(transform.position, controlPoints[segment_number]) > 1)
+		if(time/num < 1)
+		{
+            Vector3 temp = ComputePointOnCatmullRomCurve(time/num, segment_number);
             transform.position = temp;
         }
         else
         {
 			Debug.Log(segment_number);
             time = 0;
-            if (segment_number < 7)
+            if (segment_number < NumberOfPoints - 1)
             {
 				segment_number += 1;
             }
